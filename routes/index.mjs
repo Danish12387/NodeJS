68d3e8caf8ea9a8a854a4a3121d48474a28b9381
Syed Muhammad Danish <135580119+Danish12387@users.noreply.github.com>
2024-02-20 22:42:46 +0500

    updated

---
+++ routes/index.mjs
@@ -0,0 +1,12 @@
+import express from 'express';
+import products from './products.mjs';
+
+const router = express.Router();
+
+router.use('/products', products);
+
+router.use('/', (req, res) => {
+    res.send({message: "This is localhost 3001"});
+})
+
+export default router;
