a6d7a406dc8f8319bf3693f714d6b646a7cf1ad8
Syed Muhammad Danish <135580119+Danish12387@users.noreply.github.com>
2024-02-22 21:36:21 +0500

    updated

--- index.mjs
+++ index.mjs
@@ -1,10 +1,13 @@
 import express from 'express';
 import routes from './routes/index.mjs';
+import bodyParser from 'body-parser';
 
 const app = express()
 
 app.listen(3001, function(){
     console.log('server is running at port 3001.')
 })
+
+app.use(bodyParser.json());
 
 app.use('/', routes)
--- routes/products.mjs
+++ routes/products.mjs
@@ -2,10 +2,10 @@ import express from 'express';
 
 const router = express.Router();
 
-const products = [{"id":1,"title":"iPhone 9","description":"An apple mobile which is nothing like apple","price":549,"discountPercentage":12.96,"rating":4.69,"stock":94,"brand":"Apple","category":"smartphones","thumbnail":"https://cdn.dummyjson.com/product-images/1/thumbnail.jpg","images":["https://cdn.dummyjson.com/product-images/1/1.jpg","https://cdn.dummyjson.com/product-images/1/2.jpg","https://cdn.dummyjson.com/product-images/1/3.jpg","https://cdn.dummyjson.com/product-images/1/4.jpg","https://cdn.dummyjson.com/product-images/1/thumbnail.jpg"]},{"id":2,"title":"iPhone X","description":"SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...","price":899,"discountPercentage":17.94,"rating":4.44,"stock":34,"brand":"Apple","category":"smartphones","thumbnail":"https://cdn.dummyjson.com/product-images/2/thumbnail.jpg","images":["https://cdn.dummyjson.com/product-images/2/1.jpg","https://cdn.dummyjson.com/product-images/2/2.jpg","https://cdn.dummyjson.com/product-images/2/3.jpg","https://cdn.dummyjson.com/product-images/2/thumbnail.jpg"]}]
+const products = [{ "id": 1, "title": "iPhone 9", "description": "An apple mobile which is nothing like apple", "price": 549, "discountPercentage": 12.96, "rating": 4.69, "stock": 94, "brand": "Apple", "category": "smartphones", "thumbnail": "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg", "images": ["https://cdn.dummyjson.com/product-images/1/1.jpg", "https://cdn.dummyjson.com/product-images/1/2.jpg", "https://cdn.dummyjson.com/product-images/1/3.jpg", "https://cdn.dummyjson.com/product-images/1/4.jpg", "https://cdn.dummyjson.com/product-images/1/thumbnail.jpg"] }, { "id": 2, "title": "iPhone X", "description": "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...", "price": 899, "discountPercentage": 17.94, "rating": 4.44, "stock": 34, "brand": "Apple", "category": "smartphones", "thumbnail": "https://cdn.dummyjson.com/product-images/2/thumbnail.jpg", "images": ["https://cdn.dummyjson.com/product-images/2/1.jpg", "https://cdn.dummyjson.com/product-images/2/2.jpg", "https://cdn.dummyjson.com/product-images/2/3.jpg", "https://cdn.dummyjson.com/product-images/2/thumbnail.jpg"] }]
 
 router.get('/', (req, res) => {
-    res.send({message: 'Products fetched successfully!', products})
+    res.send({ message: 'Products fetched successfully!', products })
 })
 
 // products.map((item)=>{
@@ -14,12 +14,34 @@ router.get('/', (req, res) => {
 //     })
 // })
 
-router.get('/:id', (req, res) => {
-   products.map((item)=>{
-    if(req.params.id === item.id) {
-       res.send(item)
-    }
-   })
+products.map((item) => {
+    router.get('/:id', (req, res) => {
+        if (req.params.id == item.id) {
+            res.send({ message: 'Single product fetched successfully', data: item })
+        } else {
+            res.send({ message: `Cannot find data of this '${req.params.id}' id!` });
+        }
+    })
+})
+
+router.post('/add', (req, res) => {
+    console.log(req.body)
+    res.send({ message: "Product added successfully!" })
+})
+
+router.put('/update', (req, res) => {
+    console.log(req.body)
+    res.send({ message: "Product updated successfully!" })
+})
+
+products.map((item) => {
+    router.delete('/:id', (req, res) => {
+        if (req.params.id == item.id) {
+            res.send({ message: 'Product deleted successfully', deletedProduct: item })
+        } else {
+            res.send({ message: `Cannot find data of this '${req.params.id}' id!` });
+        }
+    })
 })
 
 export default router;
